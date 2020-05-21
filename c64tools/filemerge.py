623d8a78f51b0fe6e574729717e8653ec8f1f30d
dkrajzew <d.krajzewicz@gmail.com>
2020-05-22 00:23:05 +0200

    patching

--- c64tools/filemerge.py
+++ c64tools/filemerge.py
@@ -19,35 +19,47 @@ https://github.com/dkrajzew/c64tools
 
 Available under LGPL 3.0, all rights reserved
 """
+
+
+# --- imports -------------------------------------------------------
 import sys
 from optparse import OptionParser
 
 
-optParser = OptionParser(usage="""usage:\n  %prog <MEMORY_DUMP>\n  %prog [options]""")
-optParser.add_option("-f", "--file", dest="file", default=None, help="Defines the files to load, separated by a ','")
-optParser.add_option("-o", "--output", dest="output", default=None, help="Defines the name of the file to write")
-options, remaining_args = optParser.parse_args(args=args)
-if not options.file:
-  optParser.error("no input file(s) given...")
-  sys.exit()
 
-files = options.file.split(",")
-mem = bytearray([0]*65536)
-minAddr = 65536
-maxAddr = 0
-for f in files:
-  fd = open(f, "rb")
-  vals = bytearray(fd.read())
+# --- methods -------------------------------------------------------
+# -- main
+def main(args):
+  """Initialises an empty memory, loads subsequently the defined files
+  to their addresses and writes the memory as occupied by the loaded
+  contents."""
+  optParser = OptionParser(usage="""usage:\n  %prog <MEMORY_DUMP>\n  %prog [options]""")
+  optParser.add_option("-f", "--file", dest="file", default=None, help="Defines the files to load, separated by a ','")
+  optParser.add_option("-o", "--output", dest="output", default=None, help="Defines the name of the file to write")
+  options, remaining_args = optParser.parse_args(args=args)
+  if not options.file:
+    optParser.error("no input file(s) given...")
+    sys.exit()
+  files = options.file.split(",")
+  mem = bytearray([0]*65536)
+  minAddr = 65536
+  maxAddr = 0
+  for f in files:
+    fd = open(f, "rb")
+    vals = bytearray(fd.read())
+    fd.close()
+    addr = vals[0] + 256 * vals[1]
+    minAddr = min(minAddr, addr)
+    maxAddr = max(maxAddr, addr+len(vals)-2)
+    mem = mem[:addr] + bytearray(vals[2:]) + mem[addr+len(vals)-2:]
+  fd = open(options.output, "wb")
+  print (minAddr)
+  fd.write(bytearray([minAddr%256, int(minAddr/256)]))
+  fd.write(mem[minAddr:maxAddr])
   fd.close()
-  addr = vals[0] + 256 * vals[1]
-  minAddr = min(minAddr, addr)
-  maxAddr = max(maxAddr, addr+len(vals)-2)
-  mem = mem[:addr] + bytearray(vals[2:]) + mem[addr+len(vals)-2:]
-fd = open(options.output, "wb")
-print minAddr
-fd.write(bytearray([minAddr%256, int(minAddr/256)]))
-fd.write(mem[minAddr:maxAddr])
-fd.close()
 
 
+# -- main check
+if __name__ == "__main__":
+  main(sys.argv)
  
