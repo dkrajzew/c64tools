b3d31a2677a05daaef4a8ff52e764b830bf396c7
dkrajzew <d.krajzewicz@gmail.com>
2020-05-22 00:24:17 +0200

    patching documentation

---
+++ build_docs.bat
@@ -0,0 +1,6 @@
+python c:\Python27\Lib\pydoc.py -w c64tools\c64tools.py
+python c:\Python27\Lib\pydoc.py -w c64tools\charpacker.py
+python c:\Python27\Lib\pydoc.py -w c64tools\charset2png.py
+python c:\Python27\Lib\pydoc.py -w c64tools\filemerge.py
+python c:\Python27\Lib\pydoc.py -w c64tools\mem2png.py
+
