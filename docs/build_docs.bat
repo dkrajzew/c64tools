500d3528603451129f3260d64315a4486001afdc
dkrajzew <d.krajzewicz@gmail.com>
2023-02-02 21:24:30 +0100

    adding mkdocs building

--- docs/build_docs.bat
+++ docs/build_docs.bat
@@ -3,4 +3,5 @@ python c:\Python27\Lib\pydoc.py -w c64tools\charpacker.py
 python c:\Python27\Lib\pydoc.py -w c64tools\charset2png.py
 python c:\Python27\Lib\pydoc.py -w c64tools\filemerge.py
 python c:\Python27\Lib\pydoc.py -w c64tools\mem2png.py
+mkdocs build
 
