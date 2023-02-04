df7786bf962f3796f1903ec4f6b7eef70cd1099e
dkrajzew <d.krajzewicz@gmail.com>
2023-02-04 22:23:22 +0100

    debugging docs building

--- docs/build_docs.bat
+++ docs/build_docs.bat
@@ -1,8 +1,8 @@
 set PYTHONPATH=%PYTHONPATH%;..\src\
-python C:\Python38\Lib\pydoc.py -w ..\src\c64tools.py
-python C:\Python38\Lib\pydoc.py -w ..\src\charpacker.py
-python C:\Python38\Lib\pydoc.py -w ..\src\charset2png.py
-python C:\Python38\Lib\pydoc.py -w ..\src\filemerge.py
-python C:\Python38\Lib\pydoc.py -w ..\src\mem2png.py
+python C:\Python38\Lib\pydoc.py -w ..\c64tools\__init__.py
+python C:\Python38\Lib\pydoc.py -w ..\c64tools\charpacker.py
+python C:\Python38\Lib\pydoc.py -w ..\c64tools\charset2png.py
+python C:\Python38\Lib\pydoc.py -w ..\c64tools\filemerge.py
+python C:\Python38\Lib\pydoc.py -w ..\c64tools\mem2png.py
 mkdocs build
 
